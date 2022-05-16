6740cd6d20d2331652383a102634b0acf5046b8e
Yusuf-bek <ogabeksherakhmatov@gmail.com>
2022-05-16 14:01:08 +0500

    problem 184

---
+++ bin/easy/problem184.dart
@@ -0,0 +1,7 @@
+// Question:
+// https://mail.google.com/mail/u/0/#inbox/FMfcgzGpFqdCGZVVfRwNPvtsbVkNtvKm
+
+num problem184(List<num> nums) {
+  nums.sort();
+  return nums[0];
+}
---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'easy/problem184.dart';
+
+void main(List<String> arguments) {
+  print(problem184([13, 4.3, 54, 65, 32, 59]));
+}
